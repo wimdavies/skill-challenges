6447854334ca6c2cd9d422b9c7ff4d333e947b6c
wimdavies <william.johnston.davies@gmail.com>
2023-04-14 14:03:59 +0100

    working on getting visibility in debug_2.rb

--- debugging/debug_2.rb
+++ debugging/debug_2.rb
@@ -1,18 +1,26 @@
 def encode(plaintext, key)
+  puts "At this point, plaintext is #{plaintext} and key is #{key}"
+
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  puts "#{cipher}"
+
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
   return ciphertext_chars.join
 end
 
-def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
-  end
-  return plaintext_chars.join
-end
+puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+
+
+
+# def decode(ciphertext, key)
+#   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+#   plaintext_chars = ciphertext.chars.map do |char|
+#     cipher[65 - char.ord]
+#   end
+#   return plaintext_chars.join
+# end
 
 # Intended output:
 #
