57152c29ef7d8c3d86ea04439af6fe0f44d94308
Christie <christiegriffiths@MASTAFF-Mel-Gladwin-C17GMAU4Q6L4.local>
2023-04-17 14:14:24 +0100

    grammar_stats working on percentage_good

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,10 +1,11 @@
 class GrammarStats
   def initialize
-    # ...
+    @texts = []
   end
 
   def check(text)
     fail "Text string must not be empty" if text.empty?
+    @texts << text
     start_cap = (text[0] == text[0].upcase)
     valid_endings = [".", "?", "!"]
       if start_cap && (valid_endings.include? text[-1])
@@ -19,6 +20,11 @@ class GrammarStats
     # letter and ends with a sentence-ending punctuation mark.
 
   def percentage_good
+    fail "No texts checked yet" if @texts.empty?
+    0
+
+
+
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
   end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -25,12 +25,33 @@ RSpec.describe GrammarStats do
       end
     end
 
-    context "returns false when"
+    context "returns false when" do 
       it "given no cap and no punctuation" do
         new_stats = GrammarStats.new
         expect(new_stats.check("hawaii")).to eq false
       end
-
+    end
   end
+
+  describe "#percentage_good" do 
+    it "fails when asked to check 0 texts" do 
+      new_stats = GrammarStats.new
+      expect { new_stats.percentage_good }.to raise_error "No texts checked yet"
+    end
+
+    it "returns 0 when all texts have returned false" do 
+      new_stats = GrammarStats.new
+      new_stats.check("hawaii")
+      expect(new_stats.percentage_good).to eq 0
+    end 
+    
+    it "returns 100 when all texts have returned true" do 
+      new_stats = GrammarStats.new
+      new_stats.check("Hawaii.")
+      expect(new_stats.percentage_good).to eq 100
+    end 
+  end 
+
+
 
 end
