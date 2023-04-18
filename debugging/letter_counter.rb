d6aaaf4d4ba4528ec23444d9cc1bcc718c31cd4b
Christie <christiegriffiths@MASTAFF-Mel-Gladwin-C17GMAU4Q6L4.local>
2023-04-18 16:28:51 +0100

    letter_counter 70% completed

--- debugging/letter_counter.rb
+++ debugging/letter_counter.rb
@@ -4,19 +4,19 @@ class LetterCounter
   end
 
   def calculate_most_common()
-    counter = Hash.new(1)
+    counter = Hash.new(0)
     most_common = nil
-    most_common_count = 1
-    binding.irb
+    most_common_count = 0
+
     @text.chars.each do |char|
       next unless is_letter?(char)
-      counter[char] = (counter[char] || 1) + 1
-      binding.irb
+      counter[char] += 1
+      
       if counter[char] > most_common_count
         most_common = char
         most_common_count += counter[char]
       end
-      binding.irb
+    
     end
 
     return [most_common_count, most_common]
@@ -25,7 +25,8 @@ class LetterCounter
   private
 
   def is_letter?(letter)
-    return letter =~ /[a-z]/i
+    check = letter =~ /[a-z]/i
+    check != nil
   end
 end
 
