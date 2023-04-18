8107ad6153975130bdd3e236e4bbd939bc8a8b79
wimdavies <william.johnston.davies@gmail.com>
2023-04-18 14:23:01 +0100

    debugging vowel_remover.rb

--- lib/vowel_remover.rb
+++ lib/vowel_remover.rb
@@ -7,12 +7,16 @@ class VowelRemover
   def remove_vowels()
     i = 0
     while i < @text.length()
-      # binding.irb
+      binding.irb
       if @vowels.include? @text[i].downcase
         @text = @text.slice(0,i) + @text.slice(i+1..-1)
+        binding.irb
       end
       i += 1
     end
     return @text
   end
 end
+
+remover = VowelRemover.new("E will remove the vowels from this sentence.")
+puts remover.remove_vowels
