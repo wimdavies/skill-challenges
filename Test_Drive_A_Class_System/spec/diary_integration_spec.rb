7056d076422209e0e6aa83535650d7b263f93ef5
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-04-19 15:32:06 +0100

    Encoding test cases

--- Test_Drive_A_Class_System/spec/diary_integration_spec.rb
+++ Test_Drive_A_Class_System/spec/diary_integration_spec.rb
@@ -1,8 +1,101 @@
-require 'diary'
-require 'diary_entry'
+require "diary"
+require "diary_entry"
 
 RSpec.describe "integration" do
-  
+  describe "#add and #all" do
+    xit "return single entry" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "my_contents")
+      my_diary.add(first_entry)
+      expect(my_diary.all).to eq [first_entry]
+    end
 
-  
+    xit "return multiple entries" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "my_contents")
+      second_entry = DiaryEntry.new("my_title", "my_contents")
+      my_diary.add(first_entry)
+      my_diary.add(second_entry)
+      expect(my_diary.all).to eq [first_entry, second_entry]
+    end
+  end
+
+  describe "#count_words" do
+    xit "returns count for single entry" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "my_contents")
+      my_diary.add(first_entry)
+      expect(my_diary.count_words).to eq 1
+    end
+
+    xit "returns count for multiple entries" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "my_contents")
+      second_entry = DiaryEntry.new("my_title", "my_contents")
+      my_diary.add(first_entry)
+      my_diary.add(second_entry)
+      expect(my_diary.count_words).to eq 2
+      find_best_entry_for_reading_time
+    end
+
+    xit "returns zero when contents are empty" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "")
+      my_diary.add(first_entry)
+      expect(my_diary.count_words).to eq 0
+    end
+
+    context "when no entries are given" do
+      xit "fails" do
+        my_diary = Diary.new
+        expect { my_diary.count_words }.to raise_error "Add at least one entry"
+      end
+    end
+  end
+
+  decribe "#reading_time" do
+    xit "return reading time for a single entry" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "one two three")
+      my_diary.add(first_entry)
+      expect(my_diary.reading_time(1)).to eq 3
+    end
+
+    xit "return reading time for multiple entries" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "one two three")
+      second_entry = DiaryEntry.new("my_title", "one two three")
+      my_diary.add(first_entry)
+      my_diary.add(second_entry)
+      expect(my_diary.reading_time(1)).to eq 6
+    end
+
+    xit "return 0 when contents are empty" do
+      my_diary = Diary.new
+      first_entry = DiaryEntry.new("my_title", "")
+      my_diary.add(first_entry)
+      expect(my_diary.reading_time(1)).to eq 0
+    end
+
+    context "when wpm is 0" do
+      xit "fails" do
+        my_diary = Diary.new
+        first_entry = DiaryEntry.new("my_title", "")
+        my_diary.add(first_entry)
+        expect { my_diary.reading_time(0) }.to raise_error "wpm must be greater than 0"
+      end
+    end
+  end
+
+  describe "#find_best_entry_for_reading_time" do
+    context "returns single best entry" do
+      xit "is given one valid entry" do
+        my_diary = Diary.new
+        first_entry = DiaryEntry.new("my_title", "one two")
+        my_diary.add(first_entry)
+        result = my_diary.find_best_entry_for_reading_time(1, 2)
+        expect(result).to eq first_entry
+      end
+    end
+  end
 end
--- Test_Drive_A_Class_System/spec/diary_spec.rb
+++ Test_Drive_A_Class_System/spec/diary_spec.rb
@@ -1,7 +1,19 @@
-require 'diary'
+require "diary"
 
 RSpec.describe Diary do
-  
+  describe "#all" do
+    xit "returns empty list when no entries added" do
+      my_diary = Diary.new
+      expect(my_diary.all).to eq []
+    end
+  end
 
-  
+  describe "#reading_time" do
+    context "when there are no entries" do
+      xit "fails" do
+        my_diary = Diary.new
+        expect { my_diary.reading_time(1) }.to raise_error "add at least one entry"
+      end
+    end
+  end
 end
